ee8f678d222b42e056bf061f6132cf7be2d28032
yoshihiro nakamura <yoshihironakamura491@gmail.com>
2021-04-03 06:24:59 +0000

    WIP:validates

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :title, length: { maximum: 3 }
+  validates :content, length: { maximum: 3 }
 end
